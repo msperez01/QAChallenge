322580c34083d50cf63997b50d7f77325564cb2f
msanchezperez1 <msperez117@gmail.com>
2019-01-18 11:21:57 -0800

    Cucumber understanding done, now for WebDriverIO

--- features/addition.feature
+++ features/addition.feature
@@ -5,6 +5,6 @@ Feature: Addition
   Addition is great as a verification exercise to get the Cucumber-js infrastructure up and running
 
   Scenario: Add two number
-    Given the numbers 1 and 3
+    Given the numbers 2 and 3
     When they are added together
-    Then the result should be 4
+    Then the result should be 5
---
+++ webdriverComponents/test.js
@@ -0,0 +1,27 @@
+//WEBDRIVER.IO TUTUTORIAL
+//https://webdriver.io/docs/gettingstarted.html
+//AWAIT:after the webdriver.io check, activate??
+//await executes the asynchronous program and fufills the "promise" then goes back into the thread
+//if promised is rejected, await throws a reject value. in this case, e
+
+//remote allows scripts to interact with the browser and the net returned information
+//3544
+// 1-18-19 error 10013 on geckodriver. port -4444 is apparently in used...also apparently a firewall issue
+const { remote } = require('webdriverio');
+
+(async () => {
+    const browser = await remote({
+        logLevel: 'error',
+        path: '/',
+        capabilities: {
+            browserName: 'firefox'
+        }
+    });
+
+    await browser.url('https://webdriver.io');
+
+    const title = await browser.getTitle();
+    console.log('Title was: ' + title);
+
+    await browser.deleteSession();
+})().catch((e) => console.error(e));
