eb74699560a50e27a4354b695c21b144c734a2fb
msanchezperez1 <msperez117@gmail.com>
2019-01-20 11:56:10 -0800

    practicing topical google search

---
+++ home.js
@@ -0,0 +1,13 @@
+//used a topical search engine based
+//source: https://developers.google.com/custom-search/docs/topical
+
+
+(function() {
+    var cx = '014194669207971106930:lgbialt57ru';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();
